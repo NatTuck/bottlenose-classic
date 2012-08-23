03a39b0ba54cebb0d2ff8023399fb64f93b43c38
Nat Tuck <nat@ferrus.net>
2012-08-23 12:51:29 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bottlenose::Application.config.secret_token = 'be751a19ac76e84ec6dd6165f4be9627d8b048b29777d7511ce05f10c0f04472803fe1e61534ebf71cf7233a8244190afbbd994b79f9d870f4053fd1dfb56f5f'
