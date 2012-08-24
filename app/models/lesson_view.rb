970cf1b4cf85afd62ff48d2e7c9bd84c45dafa06
Nat Tuck <nat@ferrus.net>
2012-08-24 10:33:31 -0400

    Add some controller scaffolding. Make tests run.

--- app/models/lesson_view.rb
+++ app/models/lesson_view.rb
@@ -1,3 +1,4 @@
 class LessonView < ActiveRecord::Base
-  attr_accessible :lesson_id, :registration_id, :viewed_page, :viewed_video, :viewed_video2
+  attr_accessible :lesson_id, :registration_id, :viewed_page, 
+                  :viewed_video, :viewed_video2
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Bottlenose::Application.routes.draw do
+  get "main/index"
+
+  resources :users
+  resources :courses
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +53,7 @@ Bottlenose::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'main#index'
 
   # See how all your routes lay out with "rake routes"
 
