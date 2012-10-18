4957e9aa1e5281922b35a60ef4653813f6f72e48
Nat Tuck <nat@ferrus.net>
2012-10-17 21:55:32 -0400

    Spelling correction. (fix #30)

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -14,6 +14,18 @@ class Lesson < ActiveRecord::Base
     unless video.nil?
       video.sub! /width="\d+"/, 'width="1120"'
       video.sub! /height="\d+"/, 'height="630"'
+
+      # Proposed changes to avoid related videos showing up after a video
+      # has played (issue #29). Not comitted due to problems with HTML5 player.
+      # 
+      # yt = Regexp.new(%Q{http://www.youtube.com/embed/(\\w+)})
+      # mm = yt.match(video)
+      # if mm
+      #   video.sub! /src="\S+"/, %Q{src="http://www.youtube.com/v/#{mm[1]}&rel=0"}
+      # end
+
+      # Another interesting idea:
+      # video.sub! /www.youtube.com/, 'www.youtube-nocookie.com'
     end
   end
 end
