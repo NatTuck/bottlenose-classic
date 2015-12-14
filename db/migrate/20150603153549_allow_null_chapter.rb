039623caed41938cca4547460fafcbd325a2d489
Nat Tuck <nat@ferrus.net>
2015-12-14 17:46:53 -0500

    Set course_id in assignments.

--- db/migrate/20150603153549_allow_null_chapter.rb
+++ db/migrate/20150603153549_allow_null_chapter.rb
@@ -1,5 +1,10 @@
 class AllowNullChapter < ActiveRecord::Migration
   def change
+    Assignment.each do |aa|
+      aa.course_id = aa.course.id
+      aa.save!
+    end
+
     change_column :assignments, :chapter_id, :integer, :null => true
     change_column :assignments, :course_id,  :integer, :null => false
   end
