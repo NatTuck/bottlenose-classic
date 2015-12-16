27360210732c45dd21e63f8b83d3233ff1ea0e92
Bottlenose Server <nat-again@example.com>
2015-12-16 18:26:25 -0500

    Add a validation that best_sub.assignment_id matches sub.assignment_id

--- app/models/best_sub.rb
+++ app/models/best_sub.rb
@@ -7,4 +7,12 @@ class BestSub < ActiveRecord::Base
   validates :user_id, :uniqueness => { :scope => :assignment_id }
 
   delegate :created_at, to: :submission
+
+  validate :submission_matches_assignment
+
+  def submission_matches_assignment
+    if submission.assignment_id != assignment_id
+      errors[:base] << "Submission / assignment mismatch."
+    end
+  end
 end
